5e90265ea56ed9bcd6b209a079d6393bf2eec1c5
AlanAlvarezAP <alan.alvarez@ucsp.edu.pe>
2026-06-15 13:07:09 -0500

    feat(evaluation): Add histograms evaluations

--- src/poker/evaluator.clj
+++ src/poker/evaluator.clj
@@ -1,18 +1,36 @@
 (ns test.evaluator
     (:require [clojure.math.combinatorics :as combo]))
 
+(defn histogram-cases [freq]
+    (cond 
+        (= freq '(4 1)) {:rank-eval 8 :type "Four of a kind"}
+        (= freq '(3 2)) {:rank-eval 7 :type "Full house"}
+        (= freq '(3 1 1)) {:rank-eval 4 :type "Three of a kind"}
+        (= freq '(2 2 1)) {:rank-eval 3 :type "Two pair"}
+        (= freq '(2 1 1 1)) {:rank-eval 2 :type "One pair"}
+        ;;Evaluation for flushes()
+        :else {:rank-eval 1 :type "High card"}))
 
-(defn histogram-evaluation [combinations]
-    (let [filter_pattern (map #(sort > (vals %)) combinations)
-        group_pattern (keys(group-by identity filter_pattern))]
-        (println "Despues de filtrar -> " filter_pattern)
-        (println "Despues de Agrupar -> " group_pattern)
+(defn histogram-evaluation [combinations all-combinations]
+    (let [format_hands (map #(let [histogram (sort > (vals %1))]
+                            {:original-hand %2
+                            :histogram histogram
+                            :evaluation-type (histogram-cases histogram)
+                            :check-draws (vec(sort >(map :rank %2)))})
+                        combinations
+                        all-combinations)
+        greater-rank (apply max (map #(:rank-eval (:evaluation-type %1)) format_hands))
+        best-hands (filter #(= greater-rank (:rank-eval (:evaluation-type %1))) format_hands)
+        best-hand-player (last(sort-by :check-draws best-hands))]
+        (println "MEJOR MANO A BUSCAR " greater-rank)
+        (println "Las mejores manos" best-hands)
+        (println "Mejor mano del jugador final" best-hand-player)
     ))
 
 
 (defn combinatories [cards-7]
     (let [all_combinations (combo/combinations cards-7 5)
-        filter_combinations (map #(frequencies (map :rank %)) all_combinations)]
+        filter_combinations (map #(frequencies (map :rank %1)) all_combinations)]
         (println "All combinations -> " all_combinations)
         (println "Only ranks -> " filter_combinations)
-        (histogram-evaluation filter_combinations)))
+        (histogram-evaluation filter_combinations all_combinations)))
